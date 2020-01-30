10c74c2d0864b4d308b4f403325cbfb2a44e3613
Julia Mantovani <julia_semef@hotmail.com>
2020-01-29 22:22:17 -0200

    Formatar documento desafio02

--- desafio02.js
+++ desafio02.js
@@ -4,12 +4,12 @@
 // Tempo mínimo de contribuição para mulheres é de 30 anos.
 // A soma da idade com o tempo de contribuição para mulheres é de no mínimo 85 anos.
 
-const name = ' Flávio ';
-const sex = ' H ';
+const name = " Flávio ";
+const sex = " H ";
 const age = 22;
-const contribuição01= 6;
-const nome = ' Neusa ';
-const sexo = ' F ';
+const contribuição01 = 6;
+const nome = " Neusa ";
+const sexo = " F ";
 const idade = 55;
 const contribuição02 = 20;
 const hcontri = 35;
@@ -20,18 +20,18 @@ const contrih = 95;
 const calculo = idade + contribuição02;
 const soma = age + contribuição01;
 
-if ((contribuição02 >= 30) || ( calculo >= 85 )) {
-    console.log(' Neusa você pode se aposentar!')
+if (contribuição02 >= 30 || calculo >= 85) {
+  console.log(" Neusa você pode se aposentar!");
 }
 
-if ((contribuição02 < 30) || ( calculo < 85 )) {
-    console.log('Neusa você não pode se aposentar ainda!')
+if (contribuição02 < 30 || calculo < 85) {
+  console.log("Neusa você não pode se aposentar ainda!");
 }
 
-if ((contribuição01 >= 35) || ( soma >= 95 )) {
-    console.log( 'Flávio você pode se aposentar!')
+if (contribuição01 >= 35 || soma >= 95) {
+  console.log("Flávio você pode se aposentar!");
 }
 
-if ((contribuição01 < 35) || ( soma < 95 )) {
-    console.log('Flávio você não pode se aposentar ainda!')
+if (contribuição01 < 35 || soma < 95) {
+  console.log("Flávio você não pode se aposentar ainda!");
 }
